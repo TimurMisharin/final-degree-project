0cc165f6b97b7684e6826c58a4d2e40b25d05fc7
Timur Misharin <misharintimur@gmail.com>
2019-11-14 01:14:43 +0200

    added auth token

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     first_name: {
@@ -43,8 +44,28 @@ const userSchema = new mongoose.Schema({
     target_name: {
         type: String,
         trim: true
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            require: true
+        }
+    }]
 })
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({
+        _id: user._id.toString()
+    }, 'qawsedrf')
+
+    user.tokens = user.tokens.concat({
+        token
+    })
+
+    await user.save()
+    return token
+}
 
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({
--- src/routers/user.js
+++ src/routers/user.js
@@ -19,7 +19,11 @@ router.post('/users', async (req, res) => {
      */
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken(user)
+        res.status(201).send({
+            user,
+            token
+        })
     } catch (e) {
         console.log('create user:', e.message)
         res.status(400).send()
@@ -29,7 +33,11 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.status(200).send(user)
+        const token = await user.generateAuthToken()
+        res.status(200).send({
+            user,
+            token
+        })
     } catch (e) {
         console.log('User Login:', e.message)
         res.status(400).send()
@@ -37,7 +45,7 @@ router.post('/users/login', async (req, res) => {
 })
 
 /**
- * get user from db
+ * get users from db
  */
 router.get('/users', async (req, res) => {
     /**
@@ -92,7 +100,7 @@ router.patch('/users/:id', async (req, res) => {
 
         res.status(200).send(user)
     } catch (e) {
-        console.log('user update:',e.message)
+        console.log('user update:', e.message)
         res.status(400).send()
     }
 })
